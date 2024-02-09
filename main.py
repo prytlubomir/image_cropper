671e9cb4892f268a215c55e8bf5e6a920d13f68e
Liubomyr Pryt <prytlubomyr@gmail.com>
2024-02-09 23:13:50 +0000

    Trying to add more ways to use program.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 '''
 Crop a few pixels from the top and bottom of each image in the selected directory.
 '''
-import os                          # interface
+# interface
+import os
+import sys
 
-from typing import Iterable, Any   # annotations
+# annotations
+from typing import Iterable, Any
 
-from PIL import Image              # image processing
+# image processing
+from PIL import Image 
 
-import config as conf              # configuration
+# configuration
+import config as conf
 
 
 def filter_files(filenames: Iterable[Any], formats: Iterable[Any]) -> list:
@@ -30,9 +35,34 @@ def crop_image(image: Image, crop_height: int) -> Image:
 
 
 if __name__ == "__main__":
+    '''
+    TODO:
+    The config file should be just a simple .txt file
+    since a .py file must be imported
+    but import in the if statement doesn't look good.
+    '''
+    # get paths to input and output directories
+    arg_paths = list(map(os.path.exists, sys.argv[1:]))
+
+    # get path to input directory
+    if len(arg_paths) > 0:
+        INPUT  = arg_paths[0]
+    elif 'conf' in globals() and 'INPUT' in conf.__dir__():
+        INPUT = conf.INPUT
+    else:
+        INPUT = input('Path to input directory: ')
+
+    if len(arg_paths) > 1:
+        OUTPUT = arg_paths[1:]
+    elif 'conf' in globals() and 'OUTPUT' in conf.__dir__():
+        OUTPUT = conf.OUTPUT
+    else:
+        print('Enter paths to output directories separated by space.')
+        OUTPUT = input(': ').split(' ')
+
 
     # get a list of image file names
-    files = os.listdir(conf.INPUT) # get the contents of the input directory
+    files = os.listdir(INPUT) # get the contents of the input directory
     files = filter_files(files, conf.IMG_FORMATS) # remove everything except images
 
     # process images
