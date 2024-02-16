c29cbd38ef5d5dda86019aa175152af9b3945439
Liubomyr Pryt <prytlubomyr@gmail.com>
2024-02-16 00:20:44 +0000

    Path arguments

--- main.py
+++ main.py
@@ -45,29 +45,24 @@ if __name__ == "__main__":
     CROP_HEIGHT = config.getint('settings', 'CROP_HEIGHT')
 
     NEW_FILE_NAMES = config.get('settings', 'NEW_FILE_NAMES')
-    '''
-    TODO:
-    The config file should be just a simple .txt file
-    since a .py file must be imported
-    but import in the if statement doesn't look good.
-    '''
+
+
+    # read command line arguments
+    # command line arguments have advantage over configuration file
+
     # get paths to input and output directories
     arg_paths = list(map(os.path.exists, sys.argv[1:]))
 
     # get path to input directory
     if len(arg_paths) > 0:
         INPUT  = arg_paths[0]
-    elif 'conf' in globals() and 'INPUT' in conf.__dir__():
-        INPUT = conf.INPUT
-    else:
+    elif INPUT == None:
         INPUT = input('Path to input directory: ')
 
+    # get path to output directory
     if len(arg_paths) > 1:
-        OUTPUT = arg_paths[1:]
-    elif 'conf' in globals() and 'OUTPUT' in conf.__dir__():
-        OUTPUT = conf.OUTPUT
-    else:
-        print('Enter paths to output directories separated by space.')
+        OUTPUT = arg_paths[1]
+    elif OUTPUT == None:
         OUTPUT = input(': ').split(' ')
 
 
